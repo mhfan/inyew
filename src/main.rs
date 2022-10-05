2847c241f2a269a1803230c8cfb9cff08ddc3122
MeiHui FAN <mhfan@ustc.edu>
2022-10-06 00:02:06 +0800

    trial work for events handler and log to browser console

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,40 @@ use yew_router::prelude::*;
     let mut _rems = poker.as_mut_slice();
     (pkns, _rems) = _rems.partial_shuffle(&mut rng, cnt);
 
+    use yew::TargetCast;
+    use web_sys::{HtmlInputElement, HtmlSelectElement, HtmlButtonElement};
+
+    let ops_selected = Callback::from(|e: Event| {
+        // You must KNOW target is a HtmlInputElement, otherwise
+        // the call to value would be Undefined Behaviour (UB).
+        //e.target_unchecked_into::<HtmlInputElement>().value();
+        //e.target().unwrap().unchecked_into::<HtmlInputElement>().value();
+
+        // When events are created the target is undefined, it's only
+        // when dispatched does the target get added.
+        // Events can bubble so this listener might catch events from child
+        // elements which are not of type HtmlInputElement
+        //e.target().and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
+        //    .map(|inp| inp.value());
+        e.target_dyn_into::<HtmlInputElement>().map(|inp| {
+            log::info!("{}", inp.get_attribute("id").unwrap());
+            inp.value()   // add value='xxx' in <input> element to change default('on')
+        });
+    });
+
+    let cnt_changed = Callback::from(|e: Event| {
+        e.target_dyn_into::<HtmlSelectElement>().map(|sel| {
+            // TODO: cnt = sel.value(); -> then 'pkns' then 'nums'
+            log::info!("{}", sel.value());  sel.value()
+        });
+    });
+
+    let num_selected = Callback::from(|e: FocusEvent| {
+        e.target_dyn_into::<HtmlButtonElement>().map(|btn| {
+            log::info!("{}", btn.inner_text());     btn.inner_text()
+        });
+    });
+
     let num_class = "px-4 py-2 m-4 w-16 text-2xl text-purple-600 font-semibold border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 shadow-xl";
     let nums = pkns.iter().map(|pkn| {
         let (num, sid) = ((pkn % 13) + 1, (pkn / 13)/* % 4 */);
@@ -41,21 +75,16 @@ use yew_router::prelude::*;
         }
     }).collect::<Html>();
 
-    let ops = [ "+", "-", "×", "÷" ].into_iter().map(|op| {
-        let ops_change = Callback::from(|e: Event| {
-            let target = e.current_target().unwrap();
-            dbg!(e); dbg!(target);
-        });
-
-        html!{ <div class="m-4">
+    let ops = [ "+", "-", "×", "÷" ].into_iter().map(|op| html!{
+        <div class="m-4">
             <input type="radio" id={ op } name="select-ops" class="hidden peer"/>
-            <label for={ op } onchange={ ops_change } class="bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 text-3xl peer-checked:outline-none peer-checked:ring-2 peer-checked:ring-indigo-500 peer-checked:ring-offset-2 peer-checked:bg-transparent rounded-md shadow-xl" data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to replace">{ op }</label>
-        </div> }
+            <label for={ op } class="bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 text-3xl peer-checked:outline-none peer-checked:ring-2 peer-checked:ring-indigo-500 peer-checked:ring-offset-2 peer-checked:bg-transparent rounded-md shadow-xl" data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to replace">{ op }</label>
+        </div>
     }).collect::<Html>();
 
     let ctrl_class = "text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-bold rounded-lg px-4 py-2 m-4";
 
-    let num_options = (4..=8).map(|n| html!{
+    let cnt_options = (4..=6).map(|n| html!{
         <option value={ n.to_string() } selected={ n == 4 }>{ format!("{n} nums") }</option>
     }).collect::<Html>();
 
@@ -63,15 +92,15 @@ use yew_router::prelude::*;
     html!{ <main>
         //<div id="play-cards"/>
 
-        <div id="ops-group" class="flex place-content-center">{ ops }</div>
+        <div id="ops-group" onchange={ ops_selected } class="flex place-content-center">{ ops }</div>
 
-        <div id="expr-skel">{ nums }
+        <div id="expr-skel" onfocus={ num_selected }>{ nums }
             <label class="text-white font-bold py-2 px-4 m-4 text-3xl rounded-md" data-bs-toggle="tooltip" title="Click to calculate">{ "≠?" }</label>
             <button class={classes!(num_class, "rounded-md")} data-bs-toggle="tooltip" title="Double click to input new goal">{ goal.to_string() }</button>
         </div>
 
         <div id="ctrl-btns">
-            <select class={classes!(ctrl_class)} data-bs-toogle="tooltip" title="Click to select numbers count">{ num_options }</select>
+            <select class={classes!(ctrl_class)} onchange={ cnt_changed } data-bs-toogle="tooltip" title="Click to select numbers count">{ cnt_options }</select>
             <button class={classes!(ctrl_class)} data-bs-toogle="tooltip" title="Click to break down selected compound expression">{ "Restore" }</button>
             <button class={classes!(ctrl_class)} data-bs-toogle="tooltip" title="Click to refresh new round game">{ "Refresh" }</button>
         </div>
@@ -139,6 +168,6 @@ fn root_route(routes: &Route) -> Html {
 }
 
 fn main() {     // entry point
-    //wasm_logger::init(wasm_logger::Config::default()); //log::info!("Update: {:?}", msg);
+    wasm_logger::init(wasm_logger::Config::default()); //log::info!("Update: {:?}", msg);
     yew::start_app::<App>();
 }
