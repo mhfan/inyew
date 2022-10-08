6ff3e7be0e63411d80325fe8ca6e5ae10918ac0a
MeiHui FAN <mhfan@ustc.edu>
2022-10-08 22:19:41 +0800

    randomly refined

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ use yew_router::prelude::*;
     #[at("/inyew/404")] #[not_found] NotFound,
 }
 
-use web_sys::{HtmlInputElement, HtmlSelectElement}; // HtmlButtonElement
+use wasm_bindgen::JsCast;
+use web_sys::{HtmlInputElement, HtmlLabelElement};
 
 struct Game24 {
     goal: i32,
@@ -75,21 +76,15 @@ fn view(&self, ctx: &Context<Self>) -> Html {
         // elements which are not of type HtmlInputElement
         //e.target().and_then(|t| t.dyn_into::<HtmlInputElement>().ok())
         //    .map(|inp| inp.value());
+        //e.target_dyn_into::<HtmlInputElement>().map(|inp| inp.value());
 
-        let inp = e.target_dyn_into::<HtmlInputElement>().unwrap();
-        //inp.value()   // add value='xxx' in <input> element to change default('on')
-        log::info!("{}", inp.get_attribute("id").unwrap());
+        let inp = e.target().unwrap().dyn_into::<HtmlInputElement>().unwrap();
+        log::info!("{}", inp.value());  // require value='xxx' in <input>, default is 'on'
         Msg::Operator(inp)
     });
 
-    let _ops_chkclear = Callback::from(|e: MouseEvent| {
-        let inp = e.target_dyn_into::<HtmlInputElement>().unwrap();
-        log::info!("checked: {}", inp.checked());
-        if inp.checked() { inp.set_checked(false); }
-    });
-
     let cnt_changed = link.callback(|e: Event| {
-        let sel = e.target_dyn_into::<HtmlSelectElement>().unwrap();
+        let sel = e.target().unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
         log::info!("{}", sel.inner_text());     //sel.value()
         Msg::Resize(sel.value().parse::<u8>().unwrap())
     });
@@ -103,29 +98,48 @@ fn view(&self, ctx: &Context<Self>) -> Html {
     // TODO: drag to exchange/replace
 
     let num_editable = Callback::from(|e: MouseEvent| {
-        //if let Ok(Some(sel)) = web_sys::window().unwrap().document().unwrap()
-        //    .get_selection() { sel.remove_all_ranges().expect(""); }
-        let inp = e.target_dyn_into::<HtmlInputElement>().unwrap();
-        let end = inp.value().len() as u32;
-        inp.set_selection_range(end, end).expect("");   //inp.focus().expect("");
-        inp.remove_attribute("readonly").expect("");
+        //if 1 < self.cnt { return }    // XXX:
+        if let Ok(label) = e.target().unwrap().dyn_into::<HtmlLabelElement>() {
+            label.set_content_editable("true");   label.focus().unwrap();
+            web_sys::window().unwrap().get_selection().unwrap().unwrap()
+                .select_all_children(label.as_ref()).expect("");
+            //label.dispatch_event(e.as_ref()).unwrap();    // XXX:
+        } else if let Ok(inp) = e.target().unwrap().dyn_into::<HtmlInputElement>() {
+            //inp.set_selection_range(end, inp.value().len() as u32).unwrap();
+            inp.remove_attribute("readonly").expect("");
+        }
     });
 
     let num_readonly = Callback::from(|e: Event| {
-        let inp = e.target_dyn_into::<HtmlInputElement>().unwrap();
-        if  inp.check_validity() { inp.set_attribute("readonly", "").expect(""); } else {
-            inp.focus().expect("");     inp.select();
-        }   log::info!("input {}", inp.value());
+        if let Ok(label) = e.target().unwrap().dyn_into::<HtmlLabelElement>() {
+            let inp = label.previous_element_sibling().unwrap().dyn_into::<HtmlInputElement>().unwrap();
+            let str = label.inner_text();
+            if  str.parse::<i32>().is_ok() {
+                label.set_content_editable("false");
+                inp.set_value(str.as_str());
+            } else {
+                //inp.dispatch_event(&Event::new("InvalidEvent").unwrap()).unwrap();   // XXX:
+                web_sys::window().unwrap().get_selection().unwrap().unwrap()
+                    .select_all_children(label.as_ref()).expect("");
+                label.focus().unwrap();
+            }   log::info!("input {}", str);
+        } else if let Ok(inp) = e.target().unwrap().dyn_into::<HtmlInputElement>() {
+            if  inp.check_validity() {
+                inp.set_attribute("readonly", "").unwrap();
+            } else {
+                inp.focus().unwrap();  inp.select();
+            }   log::info!("input {}", inp.value());
+        }
     });
 
     let num_selected = link.callback(|e: FocusEvent| {
-        let inp = e.target_dyn_into::<HtmlInputElement>().unwrap();
+        let inp = e.target().unwrap().dyn_into::<HtmlInputElement>().unwrap();
         log::info!("{}", inp.value());
         Msg::Operands(inp)
     });
 
-    let num_class = "px-4 py-2 m-4 min-w-16 w-fit bg-transparent text-center text-2xl text-purple-600 font-semibold border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 shadow-xl invalid:border-red-500";
-    let nums = self.nums.iter().map(|pkn| {
+    let num_class = "px-4 py-2 m-4 w-fit bg-transparent text-center text-2xl text-purple-600 font-semibold border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 shadow-xl invalid:border-red-500";
+    let nums = self.nums.iter().enumerate().map(|(_i, pkn)| {
         let (num, sid) = ((pkn % 13) + 1, (pkn / 13)/* % 4 */);
 
         let court = [ "T", "J", "Q", "K" ];
@@ -134,19 +148,23 @@ fn view(&self, ctx: &Context<Self>) -> Html {
             2..=9 => num.to_string(), 10..=13 => court[(num - 10) as usize].to_owned(),
             _ => "?".to_owned() }, suits[sid as usize]);     //num  // TODO:
 
-        html!{  // XXX:
+        html!{ //<div>
+            //<input type="checkbox" id={ format!("num{_i}") } value={ num.to_string() } class="hidden peer"/>
+            //<label for={ format!("num{i}") } draggable="true" class="inline-block single-line px-4 py-2 m-4 min-w-[4rem] bg-transparent text-center text-2xl text-purple-600 font-semibold border border-purple-200 rounded-full hover:text-white hover:bg-purple-600 hover:border-transparent peer-checked:outline-none peer-checked:ring-2 peer-checked:ring-purple-600 peer-checked:ring-offset-2 shadow-xl peer-invalid:border-red-500" data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to exchange\nDouble click to input new number">{ num }</label>
+            // XXX: num_selected: onchange vs onfocus, num_readonly: onblur vs onchange
             <input type="text" value={ num.to_string() } placeholder="?" inputmode="numeric" pattern=r"-?\d+" maxlength="3" size="3" draggable="true" readonly=true class={ classes!(num_class, "rounded-full") } data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to exchange\nDouble click to input new number"/>
             // XXX: https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+        //</div>
         }
     }).collect::<Html>();
 
     let ops = [ "+", "-", "×", "÷" ].into_iter().map(|op| html!{ <div class="m-4">
-            <input type="radio" id={ op } name="select-ops" class="hidden peer"/>
-            <label for={ op } draggable="true" class="bg-indigo-600 hover:bg-indigo-500 text-white font-bold px-4 py-2 m-4 text-3xl peer-checked:outline-none peer-checked:ring-2 peer-checked:ring-indigo-500 peer-checked:ring-offset-2 peer-checked:bg-transparent rounded-md shadow-xl" data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to replace">{ op }</label>
+            <input type="radio" id={ op } value={ op } class="hidden peer"/>
+            <label for={ op } draggable="true" class="px-4 py-2 m-4 bg-indigo-600 text-white text-3xl font-bold hover:bg-indigo-500 peer-checked:outline-none peer-checked:ring-2 peer-checked:ring-indigo-500 peer-checked:ring-offset-2 peer-checked:bg-transparent rounded-md shadow-xl" data-bs-toggle="tooltip" title="Click to select/unselect\nDrag over to replace">{ op }</label>
         </div>
     }).collect::<Html>();
 
-    let ctrl_class = "text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-bold rounded-lg px-4 py-2 m-4";
+    let ctrl_class = "px-4 py-2 m-4 text-gray-900 font-bold bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 rounded-lg hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80";
 
     let cnt_options = (4..=6).map(|n| html!{
         <option value={ n.to_string() } selected={ n == self.nums.len() }>{ format!("{n} nums") }</option>
@@ -158,17 +176,23 @@ fn view(&self, ctx: &Context<Self>) -> Html {
 
         <div id="ops-group" onchange={ ops_selected } class="flex place-content-center">{ ops }</div>
 
-        <div id="expr-skel">    // XXX: class="select-none" user-select="none" why not working?
-            <span id="num-operands" onfocus={ num_selected } ondblclick={ num_editable.clone() } onchange={ num_readonly.clone() }>{ nums }</span>
+        <div id="expr-skel" class="flex place-content-center">
+            <style>{ r"
+                [contenteditable='true'].single-line { white-space: nowrap; overflow: hidden; } 
+                [contenteditable='true'].single-line br { display: none; }
+                [contenteditable='true'].single-line  * { display: inline; white-space: nowrap; } 
+            " }</style>
+
+            <div id="num-operands" class="flex place-content-center" onfocus={ num_selected } ondblclick={ num_editable.clone() } onchange={ num_readonly.clone() }>{ nums }</div>
 
             // data-bs-toggle="collapse" data-bs-target="#all-solutions" aria-expanded="false" aria-controls="all-solutions"
-            <button class="text-white font-bold py-2 px-4 m-4 text-3xl rounded-md  hover:outline-none hover:ring-2 focus:ring-indigo-500 active:ring-offset-2" data-bs-toggle="tooltip" title="Click to show solutions">{ "≠?" }</button>
-            <input type="text" value={ self.goal.to_string() } placeholder="??" inputmode="numeric" pattern=r"-?\d+" maxlength="4" size="4" readonly=true name="goal" ondblclick={ num_editable } onchange={ num_readonly } class={ classes!(num_class, "rounded-md") } data-bs-toggle="tooltip" title="Double click to input new goal"/>
+            <button class="py-2 px-4 m-4 text-white text-3xl font-bold rounded-md hover:outline-none hover:ring-2 focus:ring-indigo-500 active:ring-offset-2" data-bs-toggle="tooltip" title="Click to show solutions">{ "≠?" }</button>
+            <input type="text" value={ self.goal.to_string() } placeholder="??" inputmode="numeric" pattern=r"-?\d+" maxlength="4" size="4" readonly=true ondblclick={ num_editable } onchange={ num_readonly } class={ classes!(num_class, "rounded-md") } data-bs-toggle="tooltip" title="Double click to input new goal"/>
         </div>
 
         <div id="ctrl-btns">
             <select class={ classes!(ctrl_class) } onchange={ cnt_changed } data-bs-toogle="tooltip" title="Click to select numbers count">{ cnt_options }</select>
-            <input type="reset" value={ "Restore" } onclick={ restore } class={ classes!(ctrl_class) } data-bs-toogle="tooltip" title="Click to break down selected compound expression"/> //{ "Restore" } </button>    // XXX:
+            <input type="reset" value={ "Restore" } onclick={ restore } class={ classes!(ctrl_class) } data-bs-toogle="tooltip" title="Click to break down selected compound expression"/> //{ "Restore" } </button>
             <button class={ classes!(ctrl_class) } onclick={ new_refresh } data-bs-toogle="tooltip" title="Click to refresh new round game">{ "Refresh" }</button>
         </div>
 
@@ -185,13 +209,21 @@ fn view(&self, ctx: &Context<Self>) -> Html {
                 if  self.elem_na != None && self.elem_op != None {
                     let op = self.elem_op.as_ref().unwrap();
                     let na = self.elem_na.as_ref().unwrap();
-                    if  na.is_same_node(Some(inp.as_ref())) { return false }
-                    let str = format!("({} {} {})", na.value(),
-                        op.get_attribute("id").unwrap(), inp.value());
+                    if inp.is_same_node(Some(na.as_ref())) { return false }
+                    let str = format!("({} {} {})", na.value(), op.value(), inp.value());
+                    log::info!("{str}");
 
-                    inp.set_max_length(str.len() as i32);
-                    inp.set_size (str.len() as u32);
-                    inp.set_value(str.as_str());    log::info!("{str}");
+                    if let Ok(label) = inp.next_element_sibling().unwrap().dyn_into::<HtmlLabelElement>() {
+                        label.set_inner_text(str.as_str());
+
+                        let label =  na.next_element_sibling().unwrap().dyn_into::<HtmlLabelElement>().unwrap();
+                        label.set_hidden(true);
+                    } else {
+                        inp.set_max_length(str.len() as i32);
+                        inp.set_size (str.len() as u32);
+                        inp.set_value(str.as_str());
+                         na.set_hidden(true);
+                    }
 
                     self.cnt += 1;  /* if self.cnt == 2 {
                         // TODO: disable text input for all numbers
@@ -199,23 +231,28 @@ fn view(&self, ctx: &Context<Self>) -> Html {
                         // TODO: calculate expression in str, reflect result in equal button
                     } */
 
-                    //inp.parent_element().unwrap().remove_child(elem_na.unwrap().as_ref());
-                    op.set_checked(false);  na.set_hidden(true);
-
+                    op.set_checked(false);
                     self.elem_op = None;
                 }   self.elem_na = Some(inp);   false
             }
 
             Msg::Resize(n) => { self.dealer(n as usize);    true }
 
-            Msg::Restore => {   use wasm_bindgen::JsCast;
+            Msg::Restore => {
                 let coll = web_sys::window().unwrap().document().unwrap().get_element_by_id("num-operands").unwrap().children();
+                log::info!("{} chirldren", coll.length());
                 for i in 0..coll.length() {
-                    let inp = coll.item(i).unwrap().dyn_into::<HtmlInputElement>().unwrap();
-                    // TODO: reset numbers editable
+                    if let Ok(inp) = coll.item(i).unwrap().dyn_into::<HtmlInputElement>() {
+                        inp.set_max_length(3);  inp.set_size(3);
+                        inp.set_hidden(false);
+                    } else {
+                        let label = coll.item(i).unwrap().last_element_child().unwrap().dyn_into::<HtmlLabelElement>().unwrap();
+                        let inp = coll.item(i).unwrap().first_element_child().unwrap().dyn_into::<HtmlInputElement>().unwrap();
+                        label.set_inner_text(inp.value().as_str());
+                        label.set_hidden(false);
+                    }
 
-                    inp.set_max_length(3);  inp.set_size(3);
-                    inp.set_hidden(false);
+                    // TODO: reset numbers editable
                 }   self.cnt = 1;   true
             }
         }
