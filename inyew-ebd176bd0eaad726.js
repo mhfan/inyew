3e541fc12ed7c3301cbc852f66639df67287ac41
mhfan <mhfan@users.noreply.github.com>
2022-10-12 01:44:11 +0000

    deploy: 689ee2c26e9249deab47f5a6179e6f492fec1e88

--- inyew-ebd176bd0eaad726.js
+++ inyew-ebd176bd0eaad726.js
@@ -813,7 +813,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('inyew-1f06bf9b0403a714_bg.wasm', import.meta.url);
+        input = new URL('inyew-ebd176bd0eaad726_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
