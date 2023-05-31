e8c8ec9da77e50eb3a720af54179075f7e42ede4
mhfan <mhfan@users.noreply.github.com>
2023-05-31 11:14:25 +0000

    deploy: b2c1e1fe29a75ecc24c860b4f084be8e27afd793

--- inyew-98d5ad24f85003ff.js
+++ inyew-98d5ad24f85003ff.js
@@ -945,7 +945,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('inyew-2a412f16b5865cfd_bg.wasm', import.meta.url);
+        input = new URL('inyew-98d5ad24f85003ff_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
